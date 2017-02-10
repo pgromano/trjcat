1f81156058ba1858c9ca2b4799ee6a25009ad561
Pablo Romano <pgromano@users.noreply.github.com>
2017-02-10 10:50:02 -0800

    pass name template for general file matching

--- set_time.py
+++ set_time.py
@@ -1,8 +1,12 @@
 import numpy as np
 from glob import glob
+from sys import argv
+
+file_template = str(argv[1])
+N = len(file_template)
 
 f = open('settime.inp', 'w')
 files = sorted(glob('*.xtc'))
 for file in files:
-	f.write('%sn; % file[4:-4])
+	f.write('%sn; % file[N:-4])
 f.close()
